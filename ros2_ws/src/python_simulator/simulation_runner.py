478c7f6d0eb69c1bd7d98319c3c1ad95171997c7
mariedubro <marie.dubromel@ensta-bretagne.org>
2025-01-31 12:32:47 +0100

    newtopicsimu

--- ros2_ws/src/python_simulator/simulation_runner.py
+++ ros2_ws/src/python_simulator/simulation_runner.py
@@ -1,45 +1,72 @@
-from calcul_tools import *
-from draw import *
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Header
+from BoxStamped.msg import Box
+from Interval.msg import Interval
+from datetime import datetime
 from simu import Simulation
 from boat import Boat
 from buoy import Buoy
+from calcul_tools import *
+from draw import *
 
+
+class PositionPublisher(Node):
+    def __init__(self):
+        super().__init__('position_publisher')
+        self.publisher_ = self.create_publisher(Box, '/it/position', 10)
+
+    def publish_position(self, x, y, z=0.0):
+        # Create the intervals for x, y, z
+        interval_x = Interval(name="x", start=x, end=x)
+        interval_y = Interval(name="y", start=y, end=y)
+        interval_z = Interval(name="z", start=z, end=z)
+
+        # Populate Box message
+        msg = Box()
+        msg.header = Header()
+        msg.header.stamp = self.get_clock().now().to_msg()
+        msg.header.frame_id = "map"
+        msg.name = "USV position"
+        msg.intervals = [interval_x, interval_y, interval_z]
+
+        self.publisher_.publish(msg)
+        self.get_logger().info(f'Published position: x=[{x}], y=[{y}], z=[{z}]')
 
 
 class SimulationRunner:
     def __init__(self):
         self.s = 8
-        self.dt = 0.1 
+        self.dt = 0.1
         self.k = 0.5
         self.Ɛ = 2
         self.num_steps = 1000
         self.record_data = False
-
+        self.position_publisher = PositionPublisher()
 
     def initialize_sea_objects(self):
         sea_objects = []
         sea_objects.append(Boat(222, -6, 2, 1.5, 0.25, 4, 1.75))
         sea_objects.append(Buoy(333, 0, 2, 0, 1, 0, 0))
-        # sea_objects.append(Ship(444, -10, -4, 1.5, 0.15))
         return sea_objects
 
-
-
     def run(self):
+        rclpy.init()  # Initialize ROS2
         sea_objects = self.initialize_sea_objects()
-        # rules, mmsi_list = self.initialize_data(sea_objects, self.rules)
         simulation = Simulation(sea_objects, self.dt, self.k)
         fig, ax = init_figure(-self.s, self.s, -self.s, self.s)
-        # fig_leg, ax_leg = init_figure(-self.s, self.s, -self.s, self.s)
-        # table = init_table(rules, self.legend, fig_leg, ax_leg)
-        if self.record_data:
-            simulation.run_with_data(self.record_data, self.num_steps, ax, self.Ɛ, self.s)
-        else:
-            simulation.run(self.record_data, self.num_steps, ax, self.Ɛ, self.s)
+
+        for step in range(self.num_steps):
+            # Clear the plot and update positions
+            simulation.run(self.record_data, 1, ax, self.Ɛ, self.s)
+
+            # Publish position for the first boat (as an example)
+            boat = sea_objects[0]  # Assuming the first object is the boat
+            self.position_publisher.publish_position(boat.x, boat.y, z=0.0)
+
+        rclpy.shutdown()  # Shutdown ROS2
 
 
-
-    # ------------------------------------------------------------------------------------------------
-
-
-
+if __name__ == "__main__":
+    runner = SimulationRunner()
+    runner.run()
